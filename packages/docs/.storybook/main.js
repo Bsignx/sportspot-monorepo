c794e116ce873fbb4d049f73e9de059b860e350e
Bruno Mariano <bsignx@gmail.com>
2023-04-02 18:45:06 -0300

    chore: config eslint in docs package

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "stories": ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  "stories": ["../src/stories/**/*.stories.mdx", "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"],
   "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
   "framework": {
     name: "@storybook/react-vite",
