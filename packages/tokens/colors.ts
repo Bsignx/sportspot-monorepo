6550f3b6ef8b2205fa14a78f7a61aa1e4965e8eb
Bruno Mariano <bsignx@gmail.com>
2023-04-01 21:21:44 -0300

    feat: add colors tokens

---
+++ packages/tokens/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  primary: "#53FFAD",
+  secondary: "#3DFFDC",
+  tertiary: "#25CAFE",
+  dark: "#212121",
+  light: "#FFFFFF",
+  grayscale100: "#EFEDE9",
+  grayscale200: "#BCBCBC",
+  grayscale300: "#8A8788",
+  gradient1:
+    "linear-gradient(274.42deg, rgba(61, 255, 220, 0.6) 0%, rgba(37, 138, 254, 0.6) 124.45%)",
+  gradient2:
+    "linear-gradient(88.66deg, #FAF7F1 46.2%, #F0EADF 143.42%, #212121 223.25%)",
+};
