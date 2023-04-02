d2db808716cb2fd45b28f87524cebf223e9bb92a
Bruno Mariano <bsignx@gmail.com>
2023-04-02 18:46:49 -0300

    chore: remove docs app

---
+++ packages/ui/src/button.tsx
@@ -0,0 +1,13 @@
+import { colors } from '@sportspot/tokens'
+
+export const Button = () => {
+  return (
+    <button
+      style={{
+        backgroundColor: colors.primary,
+      }}
+    >
+      Boop
+    </button>
+  )
+}
