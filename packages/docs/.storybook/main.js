2949650dc5c320ef9fe2b132f2e2007588ec34c9
Bruno Mariano <bsignx@gmail.com>
2023-04-09 14:28:49 -0300

    feat(docs): add addon-a11y

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,11 +1,19 @@
 module.exports = {
-  "stories": ["../src/pages/**/*.stories.mdx", "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
-  "framework": {
+  stories: [
+    "../src/pages/**/*.stories.mdx",
+    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
+  ],
+  framework: {
     name: "@storybook/react-vite",
-    options: {}
+    options: {},
   },
-  "features": {
-    "storyStoreV7": true
-  }
+  features: {
+    storyStoreV7: true,
+  },
 };
