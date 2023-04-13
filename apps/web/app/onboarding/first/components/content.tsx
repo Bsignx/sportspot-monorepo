5188826fd69868de27f2afe578d39c8ed02a6ff1
Bruno Mariano <bsignx@gmail.com>
2023-04-13 07:04:05 -0300

    feat(web): centralize logo and update bg color

--- apps/web/app/onboarding/first/components/content.tsx
+++ apps/web/app/onboarding/first/components/content.tsx
@@ -1,17 +1,21 @@
 'use client'
 
-import { Center, AbsoluteCenter } from '@sportspot/ui'
+import { Box } from '@sportspot/ui'
 
-import { MediumSizeLogo } from '../../../components/logo/medium-size-logo'
+import { MediumSizeLogo as Logo } from '../../../components/logo/medium-size-logo'
 
 const Content = () => {
   return (
-    <Center>
-      <AbsoluteCenter axis="both">
-        <h1>OnboardingFirst</h1>
-        <MediumSizeLogo />
-      </AbsoluteCenter>
-    </Center>
+    <Box h="100vh" w="100%" bg="gradient.200">
+      <Box
+        pos="absolute"
+        top="50%"
+        left="50%"
+        transform="translate(-50%, -50%)"
+      >
+        <Logo />
+      </Box>
+    </Box>
   )
 }
 
