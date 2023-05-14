a9b162cf1082e5b12d5e7d6cb4734fdb03a80c65
Bruno Mariano <bsignx@gmail.com>
2023-05-14 14:56:48 -0300

    feat(ui): add custom shadows

--- packages/ui/src/theme/foundations/index.ts
+++ packages/ui/src/theme/foundations/index.ts
@@ -2,10 +2,12 @@ import colors from './colors'
 import typography from './typography'
 import radii from './radii'
 import space from './spaces'
+import shadows from './shadows'
 
 export const foundations = {
   colors,
   radii,
   space,
+  shadows,
   ...typography,
 }
