c3ec7065cbd205593962be3dcc4d374b4add4e8e
Bruno Mariano <bsignx@gmail.com>
2023-05-22 06:48:50 -0300

    fix(web): update unused var

--- apps/web/app/components/template.tsx
+++ apps/web/app/components/template.tsx
@@ -18,7 +18,7 @@ const Template = () => {
         <>
           <SearchField apiKey={MAP_API_KEY} />
           <TileLayer
-            url={`https://api.mapbox.com/styles/v1/bsignx/clhrifzo2017c01qsh2hpcu0y/tiles/256/{z}/{x}/{y}@2x?access_token=${mapApiKey}`}
+            url={`https://api.mapbox.com/styles/v1/bsignx/clhrifzo2017c01qsh2hpcu0y/tiles/256/{z}/{x}/{y}@2x?access_token=${MAP_API_KEY}`}
             attribution='&copy; <a href="https://www.mapbox.com/">Mapbox</a>'
           />
           <Marker position={[-20.567620032412087, -48.5653164180221]}>
