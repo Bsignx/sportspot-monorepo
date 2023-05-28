391e5b106431e7c62a957f6ef73ca89c6d58f719
Bruno Mariano <bsignx@gmail.com>
2023-05-28 20:48:52 -0300

    feat(web): create spot trpc router

--- apps/web/server/api/routers/example.ts
+++ apps/web/server/api/routers/example.ts
@@ -3,14 +3,14 @@ import { z } from 'zod'
 import { createTRPCRouter, publicProcedure, protectedProcedure } from '~/server/api/trpc'
 
 export const exampleRouter = createTRPCRouter({
-  hello: publicProcedure.input(z.object({ text: z.string() })).query(({ input }) => {
+  hello: publicProcedure.input(z.object({ text: z.string().max(10) })).query(({ input }) => {
     return {
       greeting: `Hello ${input.text}`,
     }
   }),
 
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany()
+  getAllSpots: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.spot.findMany()
   }),
 
   getSecretMessage: protectedProcedure.query(() => {
