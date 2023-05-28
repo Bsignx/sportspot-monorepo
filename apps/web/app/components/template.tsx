e747ab0996c4e9ba46012481bdec306216146be2
Bruno Mariano <bsignx@gmail.com>
2023-05-28 20:52:04 -0300

    feat(web): add spots markers and integrate with api

--- apps/web/app/components/template.tsx
+++ apps/web/app/components/template.tsx
@@ -1,10 +1,12 @@
 'use client'
 
 import dynamic from 'next/dynamic'
+import Leaflet from 'leaflet'
 
 import Map from '~/components/map'
 import { env } from '~/env.mjs'
 import { useGetUserLocation } from '~/hooks/useGetUserLocation'
+import { api } from '~/helpers/trpc/api'
 
 const SearchField = dynamic(() => import('../../components/map/search-field'), {
   ssr: false,
@@ -15,12 +17,16 @@ const DEFAULT_LATITUDE = -20.567620032412087
 const DEFAULT_LONGITUDE = -48.5653164180221
 
 const Template = () => {
+  const { data: spots, isError } = api.spot.getAllSpots.useQuery()
+
   const { location } = useGetUserLocation()
 
   const coords = [
     location?.coords.latitude || DEFAULT_LATITUDE,
     location?.coords.longitude || DEFAULT_LONGITUDE,
   ]
+
+  if (isError) return <div>Erro ao carregar os spots</div> // TODO: create error toast
 
   return (
     <Map center={coords} zoom={12}>
@@ -36,6 +42,27 @@ const Template = () => {
               A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
           </Marker>
+          {spots?.map((spot) => (
+            <Marker
+              key={spot.id}
+              position={[spot.latitude, spot.longitude]}
+              icon={
+                new Leaflet.Icon({
+                  iconUrl: `/images/leaflet/tags/${spot.tagName}.png`,
+                  shadowUrl: `/images/leaflet/marker-shadow.png`,
+                  iconSize: [32, 38],
+                  iconAnchor: [12, 41],
+                  popupAnchor: [1, -34],
+                  tooltipAnchor: [16, -28],
+                  shadowSize: [41, 41],
+                })
+              }
+            >
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          ))}
         </>
       )}
     </Map>
