2b75a7c245a198fdc3f8f293ffbb00519b8f07c3
Bruno Mariano <bsignx@gmail.com>
2023-09-22 14:15:47 -0300

    feat(mobile): add StatusBar style as light

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,16 +1,19 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet } from 'react-native'
 import WebView from 'react-native-webview'
 
 export default function App() {
   return (
-    <WebView
-      style={styles.container}
-      userAgent="Mozilla/5.0 (Linux; Android 10; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.210 Mobile Safari/537.36"
-      geolocationEnabled
-      forceDarkOn
-      source={{ uri: 'http://192.168.2.4:3000/' }}
-    />
+    <>
+      <StatusBar style="light" />
+      <WebView
+        style={styles.container}
+        userAgent="Mozilla/5.0 (Linux; Android 10; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.210 Mobile Safari/537.36"
+        geolocationEnabled
+        forceDarkOn
+        source={{ uri: 'http://192.168.2.4:3000/' }}
+      />
+    </>
   )
 }
 
@@ -20,6 +23,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#2a2a2a',
     alignItems: 'center',
     justifyContent: 'center',
-    // marginTop: 36,
   },
 })
